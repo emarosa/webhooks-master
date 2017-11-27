355f8cc05d036bbce88823a5ed3fbb55df065699
ian fireman <imlbf@cin.ufpe.br>
2017-11-26 23:57:36 -0300

    remove cookie

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var webhooks = require('./routes/webhooks');
@@ -9,7 +8,6 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use('/webhooks', webhooks);
 
