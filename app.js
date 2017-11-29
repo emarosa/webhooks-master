44048e1a7560087f307b7dc923cd3b18ba3319f1
ian fireman <imlbf@cin.ufpe.br>
2017-11-28 22:48:49 -0300

    add something to test webhook

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var webhooks = require('./routes/webhooks');
+var port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/webhooks', webhooks);
 
-app.listen(3000, function () {
-    console.log('Running webhook master!');
+app.listen(port, function () {
+    console.log('Running webhook master on port ' + port + '!');
 });
